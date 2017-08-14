b18dab5a2041cf46fd218a60d5667c0e2062d489
iaintshine <bodziomista@gmail.com>
2017-08-14 15:44:05 +0200

    Tests for CompositeLogger

--- lib/tracing/composite_logger.rb
+++ lib/tracing/composite_logger.rb
@@ -1,12 +1,14 @@
 module Tracing
   class CompositeLogger < ::Logger
+    attr_reader :destinations
+
     def initialize(*loggers)
       super(nil)
-      @loggers = loggers
+      @destinations = loggers
     end
 
     def add(*args, &block)
-      @loggers.each { |logger| logger.add(*args, &block) }
+      @destinations.each { |logger| logger.add(*args, &block) }
     end
   end
 end
